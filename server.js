e40807ccdf2bb2fa616ea292670e18f9c6b8a0f6
Nelson-wedefineapps <nelson@wedefineapps.com>
2024-09-28 21:36:32 +0530

    adding test pushes

--- server.js
+++
@@ -1,9 +0,0 @@
-const http =require('node:http')
-
-const server = http.createServer((req,res)=>{
-    res.end("Sample response")
-})
-
-
-
-server.listen(3001,()=>{console.log("[!] Server started");})
